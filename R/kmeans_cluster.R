cbf6e9a12e8e7cd3aebecd8f8f0604717a35223e
jbhender <zhaoleo@umich.edu>
2021-11-29 21:01:11 -0500

    Modified the documents of kmeans_cluster modified function to check whether randomly assigned the center

--- R/kmeans_cluster.R
+++ R/kmeans_cluster.R
@@ -2,46 +2,51 @@
 #'
 #'Perform k-means clustering on a data matrix.
 #'
-#'@param x numeric matrix of data, or an object that can be coerced to such a matrix (such as a numeric vector or a data frame with all numeric columns).
-#'@param nclust the maximum number of iterations allowed.
-#'@param max.iter  maximun number of interations
+#'@param X numeric matrix of data, or an object that can be coerced to such a matrix (such as a numeric vector or a data frame with all numeric columns).
+#'@param center either the number of clusters, say \(k\), or a set of initial (distinct) cluster centres. If a number, a random center will be assigned.
+#'@param max.iter the maximum number of iterations allowed.
 #'@param tol Parameters for determining convergence.
 #'
 #'@return  Contains cell cluster labels for each cell in x and the center for each cluster
 #'
 #'@examples
-#'square(3)
+#'
 #'
 #'@export
 #
 
-kmeans_cluster = function(X, nclust, max.iter, tol) {
-    X = data.matrix(X)
-    .E_step = function(){
-      SumOfSquare_diff <<- apply(mu, MARGIN = 2,FUN = function(i) rowSums((sweep(X,MARGIN = 2,FUN = "-",i))^2))
-      Cluster_index <<- apply(SumOfSquare_diff,MARGIN = 1,FUN = function(i) which.min(i))
-      SumofSquare_Loss <<- sum(t(SumOfSquare_diff)[Cluster_index + seq(0,N * nclust - nclust,nclust)])/N
-    }
-    .M_step = function(){
-      mu <<- sapply(1:nclust,FUN = function(i) colMeans(X[Cluster_index == i,]))
-    }
+kmeans_cluster = function(X, center, max.iter, tol) {
     check.tol = function(fmax,fmin,ftol){
-      delta = abs(fmax - fmin)
-      accuracy = (abs(fmax) + abs(fmin))*ftol
+      delta <- abs(fmax - fmin)
+      accuracy <- (abs(fmax) + abs(fmin))*ftol
       return(delta < (accuracy + tol))
     }
+    X <- data.matrix(X)
     N <- nrow(X)
     p <- ncol(X)
-    Cluster_index <<- rep_len(1:nclust,N)
-    mu <<- sapply(1:nclust,FUN = function(i) colMeans(X[Cluster_index == i,]))
-    SumofSquare_Loss = 10^10
+    nclust <- NULL
+    if(length(unique(center)) != 1){
+      if(is.null(dim(center))){
+        nclust <- length(center)
+      }else{
+        nclust <- ncol(center)
+      }
+      Cluster_index <- NULL
+      mu <- center
+    }else{
+      Cluster_index <- sample(1:nclust,N,replace = TRUE)
+      mu <- sapply(1:nclust,FUN = function(i) colMeans(X[Cluster_index == i,]))
+    }
+    SumofSquare_Loss <- 10^10
     for(i in 1:max.iter){
       SumofSquare_Loss0 <- SumofSquare_Loss
-      .E_step()
-      .M_step()
+      SumOfSquare_diff <- apply(mu, MARGIN = 2,FUN = function(i) rowSums((sweep(X,MARGIN = 2,FUN = "-",i))^2))
+      Cluster_index <- apply(SumOfSquare_diff,MARGIN = 1,FUN = function(i) which.min(i))
+      SumofSquare_Loss <- sum(t(SumOfSquare_diff)[Cluster_index + seq(0,N * nclust - nclust,nclust)])/N
+      mu <- sapply(1:nclust,FUN = function(i) colMeans(X[Cluster_index == i,]))
       if(check.tol(SumofSquare_Loss0,SumofSquare_Loss,tol)){
         break
       }
     }
-    return(list(mus=mu,est= Cluster_index))
+    return(list(mus <- mu,est <- Cluster_index))
 }
